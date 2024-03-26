49cbafa0ade8e92b0cfcca64f0f2e4731aaa3de5
Keskoo <moveslikevigoss@gmail.com>
2024-03-26 20:06:17 +0000

    commitment is important

---
+++ diceroll2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+int main(void)
+
+{
+    int rolls[200];
+    int sum = 0;
+    int iterations = 10;
+
+    for (int i = 0; i < iterations; i++)
+    {
+    int roll = 20;
+   // printf("Type of dice: ");
+    //scanf("%d", &roll);
+
+    srand(time(NULL));
+    int r = rand() % roll + 1;
+    rolls[i] = r;
+    printf("You rolled: %i\n", r);
+    sum += r;
+    sleep(1);
+    }
+
+    double avg = (double) sum/iterations;
+    printf("Average roll was: %f\n", avg);
+    return 0;
+}
